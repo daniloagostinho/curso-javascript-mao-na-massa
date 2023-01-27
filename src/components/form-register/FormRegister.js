5b11689f5601559b146714258b570e7be2d59378
Danilo Silva <danilo.silva@imaginebeyond.com.br>
2023-01-27 13:11:25 -0300

    Trabalhando no formulário de cadastro e criando modal de mensagem de erro campos vazios

--- src/components/form-register/FormRegister.js
+++ src/components/form-register/FormRegister.js
@@ -20,6 +20,35 @@ class FormRegister extends HTMLElement {
     }
 }
 
+let nameInputValue;
+let emailInputValue;
+let ageInputValue;
+
+const handleFormRegister = async () => {
+    nameInputValue = document.querySelector('.nameInputValue').value;
+    emailInputValue = document.querySelector('.emailInputValue').value;
+    ageInputValue = document.querySelector('.ageInputValue').value;
+
+    if(verifyEmptyValues(nameInputValue, emailInputValue, ageInputValue)) {
+        alert('Abrir modal de continuar o cadastro!');
+    } else {
+        openDialogRequireField();
+    }
+}
+
+const verifyEmptyValues = (name, email, age) => {
+    if(name !== '' && email !== '' && age !== '') {
+        return true;
+    }
+
+    return false;
+}
+
+const openDialogRequireField = () => {
+    const dialog = document.querySelector('.modal-required-field')
+    dialog.click();
+}
+
 if('customElements' in window) {
     customElements.define('app-form-register', FormRegister)
 }
---
+++ src/components/form-register/dialog-required-field/DialogRequiredField.js
@@ -0,0 +1,26 @@
+class DialogRequiredField extends HTMLElement {
+    constructor() {
+        super();
+
+        console.log('working -> ', this)
+
+        fetch('src/components/form-register/dialog-required-field/DialogRequiredField.html')
+        .then(response => response.text())
+        .then(text => this.innerHTML = text)
+
+
+    }
+
+    connectedCallback () {
+        console.log('iniciou o componente')
+    }
+
+    disconnectedCallback() {
+        console.log('destruiu o componente')
+    }
+}
+
+
+if('customElements' in window) {
+    customElements.define('app-dialog-required-field', DialogRequiredField)
+}
